13a75bb43c7440c26d56e9a943f9b654f8139bc0
Zachary Conger <zachary.conger@stackhawk.com>
2023-03-31 12:14:30 -0600

    working stackhawk config and readme

---
+++ README_STACKHAWK.md
@@ -0,0 +1,22 @@
+# Scanning with StackHawk
+
+Here's a quick rundown of 
+
+```shell
+# Install dependencies
+npm install
+
+# Run Juice Shop
+npm start &
+
+# Browse to juice shop at http://localhost:3000 and create the test user:
+open http://localhost:3000
+#   username: test@test.com
+#   password: testtest
+
+# Fetch a JWT for test@test.com and export it as AUTH_TOKEN - will be used for StackHawk authenticated scanning
+source stackhawk_auth_token.sh
+
+# Run HawkScan
+hawk scan
+```
--- stackhawk_auth_token.sh
+++ stackhawk_auth_token.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 AUTH_RESPONSE=$(curl -s 'http://localhost:3001/rest/user/login' -H 'Content-Type: application/json' --data-raw '{"email":"test@test.com","password":"testtest"}')
-#AUTH_RESPONSE=$(curl -s 'http://localhost:3001/rest/user/login' -H 'Content-Type: application/json' --data-raw '{"email":"admin@juice-sh.op","password":"admin123"}')
-
 export AUTH_TOKEN=$(jq -r .authentication.token <<< "${AUTH_RESPONSE}")
 
-#hawk scan
+if [[ -z $AUTH_TOKEN ]] ; then
+  echo "ERROR: No authentication token received"
+  return 1
+else
+  echo "SUCCESS: AUTH_TOKEN=${AUTH_TOKEN}"
+  return 0
+fi
