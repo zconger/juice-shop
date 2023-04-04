844f6fbd9d8fcfcff6ea134a7816f0d1ce190fdc
Zachary Conger <zachary@econger.com>
2023-04-04 14:38:29 -0600

    Update stackhawk_auth_token.sh

--- stackhawk_auth_token.sh
+++ stackhawk_auth_token.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-AUTH_RESPONSE=$(curl -s 'http://localhost:3001/rest/user/login' -H 'Content-Type: application/json' --data-raw '{"email":"test@test.com","password":"testtest"}')
+AUTH_RESPONSE=$(curl -s 'http://localhost:3000/rest/user/login' -H 'Content-Type: application/json' --data-raw '{"email":"test@test.com","password":"testtest"}')
 export JS_AUTH_TOKEN=$(jq -r .authentication.token <<< "${AUTH_RESPONSE}")
 
 if [[ -z $JS_AUTH_TOKEN ]] ; then
