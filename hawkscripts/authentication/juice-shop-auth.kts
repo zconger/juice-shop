b686d01e39d9fdcc9946acdc0a6b34e24527ad6c
Zachary Conger <zachary.conger@stackhawk.com>
2023-10-27 18:34:07 -0600

    WIP: auth script

--- hawkscripts/authentication/juice-shop-auth.kts
+++ hawkscripts/authentication/juice-shop-auth.kts
@@ -6,11 +6,10 @@ import org.parosproxy.paros.network.HttpRequestHeader
 import org.zaproxy.zap.authentication.AuthenticationHelper
 import org.zaproxy.zap.authentication.GenericAuthenticationCredentials
 import com.stackhawk.hawk.Utils
+import org.zaproxy.zap.network.HttpRequestBody
 
 val logger = LogManager.getLogger("Juice Shop Auth")
 logger.info("Juice Shop authentication script starting.")
-
-val PARAM_TARGET_URL =
 
 // This function is called before a scan is started and when the loggedOutIndicator is matched indicating re-authentication is needed.
 fun authenticate(
@@ -19,24 +18,34 @@ fun authenticate(
         credentials: GenericAuthenticationCredentials
 ): HttpMessage {
 
+    val authEndpoint = "http://localhost:3000/rest/user/login"
+    val username = "test@test.com"
+    val password = "testtest"
+    val authRequestBody = "{\"email\":\"${username}\",\"password\":\"${password}\"}"
 
+    logger.info("Authentication URL: ${authEndpoint}")
 
-    logger.info("TARGET_URL: ${paramsValues[PARAM_TARGET_URL]}")
     val msg = helper.prepareMessage()
     msg.requestHeader = HttpRequestHeader(
-            HttpRequestHeader.GET, URI(paramsValues[PARAM_TARGET_URL], true),
+            HttpRequestHeader.GET,
+            URI(authEndpoint, true),
             HttpHeader.HTTP11
     )
-    logger.info("msg: $msg ${msg.requestHeader.headers.size}")
-    msg.requestHeader.headers.forEach { println(it) }
-    msg.responseHeader.setHeader()
+    msg.requestHeader.setHeader("Content-Type", "application/json")
+    msg.requestHeader.setHeader("Accept", "application/json")
+    msg.requestHeader.setHeader("Cache-control", "no-cache")
+    msg.requestBody = HttpRequestBody(authRequestBody)
+    msg.requestHeader.contentLength = msg.requestBody.length()
+
     helper.sendAndReceive(msg)
+    logger.info("Auth Request:\n=== REQUEST HEADERS ===\n${msg.requestHeader}\n=== REQUEST BODY ===\n${msg.requestBody}\n")
+    logger.info("Auth Response:\n=== RESPONSE HEADERS ===\n${msg.responseHeader}\n=== RESPONSE BODY ===\n${msg.responseBody}\n")
     return msg
 }
 
 // The required parameter names for your script, your script will throw an error if these are not supplied in the script.parameters configuration.
 fun getRequiredParamsNames(): Array<String> {
-    return arrayOf(PARAM_TARGET_URL)
+    return arrayOf()
 }
 
 // The required credential parameters, your script will throw an error if these are not supplied in the script.credentials configuration.
